21f61c381acc8c4eabe09af703d1048bc3ec8df6
Gourav Mohanty <gouravmohanty2001@gmail.com>
2023-08-06 18:05:36 +0530

    dashboard form django

--- LLM/LLM/urls.py
+++ LLM/LLM/urls.py
@@ -5,4 +5,5 @@ from LandingPage.views import landing_page
 urlpatterns = [
     path('', landing_page, name="landing_page"),
     path('accounts/', include('accounts.urls')),
+    path('', include('dashboard.urls')),
 ]
