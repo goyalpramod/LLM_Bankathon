2d573ab7da3d4b06ef65d3ba57778e2f85e2418b
Midatana Sai Praneetha <praneetha@Midatanas-MacBook-Air.local>
2023-08-06 13:06:37 +0530

    Form javascript added

---
+++ LLM_show.js
@@ -0,0 +1,10 @@
+
+function showResultBox() {
+  
+    document.getElementById('result-box').style.display = 'block';
+  }
+  
+  const jobForm = document.getElementById('job-form');
+  jobForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+  });
