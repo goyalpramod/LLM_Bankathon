a14e8dffbd5477fcf92fb8c827014ebe707a1f7a
Gourav Mohanty <gouravmohanty2001@gmail.com>
2023-08-10 16:38:27 +0530

    browse button added

--- LLM/cvDashboard/static/cvDashboard/main.js
+++ LLM/cvDashboard/static/cvDashboard/main.js
@@ -1,87 +1,89 @@
 const dropArea = document.querySelector(".drag-image"),
-dragText = dropArea.querySelector("h6"),
-button = dropArea.querySelector("button"),
-input = dropArea.querySelector("input");
-let file; 
+  dragText = dropArea.querySelector("h6"),
+  button = dropArea.querySelector("button"),
+  input = dropArea.querySelector("input");
+let file;
 
-button.onclick = ()=>{
-  input.click(); 
+button.onclick = () => {
+  input.click();
+};
+
+function displayFileName(event) {
+  const input = event.target;
+  const fileName = input.files[0].name;
+  document.getElementById("selectedFileName").textContent = fileName;
 }
 
-input.addEventListener("change", function(){
- 
+input.addEventListener("change", function () {
   file = this.files[0];
   dropArea.classList.add("active");
   viewfile();
 });
 
-dropArea.addEventListener("dragover", (event)=>{
+dropArea.addEventListener("dragover", (event) => {
   event.preventDefault();
   dropArea.classList.add("active");
   dragText.textContent = "Release to Upload File";
 });
 
-
-dropArea.addEventListener("dragleave", ()=>{
+dropArea.addEventListener("dragleave", () => {
   dropArea.classList.remove("active");
   dragText.textContent = "Drag & Drop to Upload File";
-}); 
-
-dropArea.addEventListener("drop", (event)=>{
-  event.preventDefault(); 
-   
-  file = event.dataTransfer.files[0];
-  viewfile(); 
 });
 
-function viewfile(){
-  let fileType = file.type; 
+dropArea.addEventListener("drop", (event) => {
+  event.preventDefault();
+
+  file = event.dataTransfer.files[0];
+  viewfile();
+});
+
+function viewfile() {
+  let fileType = file.type;
   let validExtensions = ["image/jpeg", "image/jpg", "image/png"];
-  if(validExtensions.includes(fileType)){ 
-    let fileReader = new FileReader(); 
-    fileReader.onload = ()=>{
-      let fileURL = fileReader.result; 
-       let imgTag = `<img src="${fileURL}" alt="image">`;
-      dropArea.innerHTML = imgTag; 
-    }
+  if (validExtensions.includes(fileType)) {
+    let fileReader = new FileReader();
+    fileReader.onload = () => {
+      let fileURL = fileReader.result;
+      let imgTag = `<img src="${fileURL}" alt="image">`;
+      dropArea.innerHTML = imgTag;
+    };
     fileReader.readAsDataURL(file);
-  }else{
+  } else {
     alert("This is not an Image File!");
     dropArea.classList.remove("active");
     dragText.textContent = "Drag & Drop to Upload File";
   }
 }
 
+const statusCell = document.querySelector(".status-cell");
+const statusText = statusCell.querySelector(".status-text");
+const statusTick = statusCell.querySelector(".status-icon.tick");
+const statusCross = statusCell.querySelector(".status-icon.cross");
+const statusPen = statusCell.querySelector(".status-icon.pen");
 
-    const statusCell = document.querySelector('.status-cell');
-    const statusText = statusCell.querySelector('.status-text');
-    const statusTick = statusCell.querySelector('.status-icon.tick');
-    const statusCross = statusCell.querySelector('.status-icon.cross');
-    const statusPen = statusCell.querySelector('.status-icon.pen');
+statusTick.addEventListener("click", () => {
+  statusText.innerHTML = "Shortlisted";
+  statusText.style.backgroundColor = "var(--color-success)";
+});
 
-    statusTick.addEventListener('click', () => {
-        statusText.innerHTML = 'Shortlisted';
-        statusText.style.backgroundColor = 'var(--color-success)';
-    });
+statusCross.addEventListener("click", () => {
+  statusText.innerHTML = "Rejected";
+  statusText.style.backgroundColor = "var(--color-danger)";
+});
 
-    statusCross.addEventListener('click', () => {
-        statusText.innerHTML = 'Rejected';
-        statusText.style.backgroundColor = 'var(--color-danger)';
-    });
+statusPen.addEventListener("click", () => {
+  const currentStatus = statusText.innerHTML;
+  if (currentStatus === "Shortlisted") {
+    statusText.innerHTML = "Rejected";
+    statusText.style.backgroundColor = "var(--color-danger)";
+  } else if (currentStatus === "Rejected") {
+    statusText.innerHTML = "Shortlisted";
+    statusText.style.backgroundColor = "var(--color-success)";
+  }
+});
 
-    statusPen.addEventListener('click', () => {
-        const currentStatus = statusText.innerHTML;
-        if (currentStatus === 'Shortlisted') {
-            statusText.innerHTML = 'Rejected';
-            statusText.style.backgroundColor = 'var(--color-danger)';
-        } else if (currentStatus === 'Rejected') {
-            statusText.innerHTML = 'Shortlisted';
-            statusText.style.backgroundColor = 'var(--color-success)';
-        }
-    });
-
-    const sendMailBtn = document.querySelector('.action-icon.send-mail');
-    sendMailBtn.addEventListener('click', () => {
-        window.open('mailto:praneetha2829@gmail.com?subject=&body=', '_blank');
-    });
-
+const sendMailBtn = document.querySelector(".action-icon.send-mail");
+sendMailBtn.addEventListener("click", () => {
+  window.open("mailto:praneetha2829@gmail.com?subject=&body=", "_blank");
+});
