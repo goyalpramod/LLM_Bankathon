96d4f35b0adc137c72cca206cd71b406947fc45d
samarthrawat1 <samarthrawat1@gmail.com>
2023-08-10 14:28:43 +0530

    edit html

--- LLM/cvDashboard/urls.py
+++ LLM/cvDashboard/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'cvDashboard'
 
 urlpatterns = [
     path('cvDashboard/', views.cv_page, name='cv_page'),
     path('job/', views.cv_page, name='cv_page'),
+    path('list/', views.upload_file, name='list'),
+
 ]
