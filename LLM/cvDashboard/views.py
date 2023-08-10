2b86dc407a10e5b4738dffac7e0bcd88bf75dad9
samarthrawat1 <samarthrawat1@gmail.com>
2023-08-10 11:59:35 +0530

    testing

--- LLM/cvDashboard/views.py
+++ LLM/cvDashboard/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
+
+
 
 def cv_page(request):
     job_title = request.session.get('job_title', 'Default Job Title')  # You can provide a default value if it's not found.
@@ -6,5 +10,17 @@ def cv_page(request):
     context = {
         'job_title': job_title
     }
-
     return render(request, 'cvDashboard/job.html', context)
+
+# from somewhere import handle_uploaded_file
+
+# def upload_file(request):
+    # if request.method == "POST":
+    #     form = UploadFileForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         handle_uploaded_file(request.FILES["file"])
+    #         return HttpResponseRedirect()
+    # else:
+    #     form = UploadFileForm()
+    # return render(request, "upload.html", {"form": form})
+    #
