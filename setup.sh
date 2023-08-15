ac62553a507c5a9e20eb96c78a5eeeddfcae839f
goyalpramod <goyalpramod1729@gmail.com>
2023-08-15 16:52:03 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,7 @@
-#!/bin/bash
-
-# Check if python is installed
-if  command  python -v &> /dev/null; then
-    echo "Error: Python is not installed."
-    exit 1
-fi
-
-# Check if pip is installed
-if ! command -v pip &> /dev/null; then
-    echo "Error: Pip is not installed."
-    exit 1
-fi
-
-# Install virtualenv if not installed
-if ! command  pip show virtualenv &> /dev/null; then
-    echo "Virtualenv is not installed. Installing..."
-    pip install virtualenv
-fi
-
 # Create and activate virtual environment
 echo "Creating and activating virtual environment..."
-virtualenv venv
-source venv/bin/activate
+python -m virtualenv .venv
+source .venv/Scripts/activate
 
 # Install requirements using pip
 echo "Installing requirements using pip..."
